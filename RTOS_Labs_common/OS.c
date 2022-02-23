12e7818216e0485960f7c3d8a665bf84da81df3e
Devangshu Goswami <devm1@Devangshus-Mac-mini.local>
2022-02-23 03:59:22 -0600

    Added SW1 task code

--- RTOS_Labs_common/OS.c
+++ RTOS_Labs_common/OS.c
@@ -310,6 +310,8 @@ void GPIOPortF_Handler(void){
  
 }
 
+void (*SW1_Task) (void);
+
 //******** OS_AddSW1Task *************** 
 // add a background task to run whenever the SW1 (PF4) button is pushed
 // Inputs: pointer to a void/void background function
@@ -324,9 +326,31 @@ void GPIOPortF_Handler(void){
 // In lab 3, there will be up to four background threads, and this priority field 
 //           determines the relative priority of these four threads
 int OS_AddSW1Task(void(*task)(void), uint32_t priority){
-  // put Lab 2 (and beyond) solution here
- 
-  return 0; // replace this line with solution
+  long volatile delay;
+	SYSCTL_RCGCGPIO_R |= 0x00000020; 	// (a) activate clock for port F
+  delay = SYSCTL_RCGCGPIO_R;
+  GPIO_PORTF_CR_R = 0x10;           // allow changes to PF4
+  GPIO_PORTF_DIR_R &= ~0x10;    		// (c) make PF4 in (built-in button)
+  GPIO_PORTF_AFSEL_R &= ~0x10;  		//     disable alt funct on PF4
+  GPIO_PORTF_DEN_R |= 0x10;     		//     enable digital I/O on PF4   
+  GPIO_PORTF_PCTL_R &= ~0x000F0000; // configure PF4 as GPIO
+  GPIO_PORTF_AMSEL_R = 0;       		//     disable analog functionality on PF
+  GPIO_PORTF_PUR_R |= 0x10;     		//     enable weak pull-up on PF4
+  GPIO_PORTF_IS_R &= ~0x10;     		// (d) PF4 is edge-sensitive
+  GPIO_PORTF_IBE_R &= ~0x10;     		//     PF4 is not edges
+	GPIO_PORTF_IEV_R &= ~0x10;     		//     PF4 falling edges
+
+	GPIO_PORTF_ICR_R = 0x10;      		// (e) clear flag4
+  GPIO_PORTF_IM_R |= 0x10;      		// (f) arm interrupt on PF4
+	
+	priority = (priority & 0x07) << 21;	// NVIC priority bit (21-23)
+	NVIC_PRI7_R = (NVIC_PRI7_R & 0xFF00FFFF); // clear priority
+	NVIC_PRI7_R = (NVIC_PRI7_R | priority); 
+  NVIC_EN0_R = 0x40000000;      		// (h) enable interrupt 30 in NVIC  
+
+  SW1_Task = task;
+
+  return 1; // replace this line with solution
 };
 
 //******** OS_AddSW2Task *************** 
